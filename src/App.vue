a65ad50aec8ad1409a75aae3ba89fad074997ba5
Kevin <seniorpepex@gmail.com>
2019-06-13 11:31:26 -0700

    got a list of rocks

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,40 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="babymon"/>
     <card></card>
+    <div v-for="(element, index) in pokemonOfRockType.pokemon" :key="index">{{element.pokemon.name}}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import card from './components/card.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import card from "./components/card.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
     card
+  },
+  data: function() {
+    return { pokemonOfRockType: "" };
+  },
+  mounted: function() {
+    const axios = require("axios");
+    const vm = this;
+    axios({
+      method: "get",
+      url: "http://pokeapi.co/api/v2/type/rock"
+    }).then(function(response) {
+      console.log(response.data.pokemon);
+      vm.pokemonOfRockType = response.data;
+    });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
